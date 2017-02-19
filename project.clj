b68a53ead6acd865c0b39adabd4e7eb8c11a314d
Paul Williams <paul@paulswilliams.me.uk>
2017-02-19 17:00:15 +0000

    added old incanter; new one caused dependency problem

--- project.clj
+++ project.clj
@@ -3,4 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [incanter/incanter "1.5.5"]
+                 [org.clojure/math.numeric-tower "0.0.4"]
+                 [me.raynes/fs "1.4.6"]])
