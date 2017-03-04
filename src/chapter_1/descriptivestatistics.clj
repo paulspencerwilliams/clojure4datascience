4a19e983439ea19a79fcda60e5de091921a523e4
Paul Williams <paul@paulswilliams.me.uk>
2017-03-04 19:14:27 +0000

    and median

--- src/chapter_1/descriptivestatistics.clj
+++ src/chapter_1/descriptivestatistics.clj
@@ -9,3 +9,17 @@
 (->> (d/load-data :uk-scrubbed)
      (i/$ "Electorate")
      (mean))
+
+(defn median [xs]
+  (let [n (count xs)
+        mid (int (/ n 2))]
+    (if (odd? n)
+      (nth (sort xs) mid)
+      (->> (sort xs)
+           (drop (dec mid))
+           (take 2)
+           (mean)))))
+
+(->> (d/load-data :uk-scrubbed)
+     (i/$ "Electorate")
+     median)
