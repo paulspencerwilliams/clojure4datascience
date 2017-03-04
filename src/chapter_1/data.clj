20cf3555eae0fb64c99a45c8fa6f0ccb19a897a1
Paul Williams <paul@paulswilliams.me.uk>
2017-03-04 16:18:08 +0000

    finished basic data ns

--- src/chapter_1/data.clj
+++ src/chapter_1/data.clj
@@ -12,3 +12,32 @@
 
 (defn uk-column-names []
   (i/col-names (load-data :uk)))
+
+(i/$ "Election Year" (load-data :uk))
+
+
+(->> (load-data :uk)
+     (i/$ "Election Year")
+     (distinct))
+
+(->> (load-data :uk)
+     (i/$ "Election Year")
+     (frequencies))
+
+(-> (load-data :uk)
+    (i/query-dataset {"Election Year" {:$eq nil}}))
+
+(->> (load-data :uk)
+     (i/$where {"Election Year" {:$eq nil}})
+     (i/to-map))
+
+(->> (load-data :uk)
+     (i/$where {"Election Year" {:$eq nil}}))
+
+(defmethod load-data :uk-scrubbed [_]
+  (->> (load-data :uk)
+       (i/$where {"Election Year" {:$ne nil}})))
+
+(->> (load-data :uk-scrubbed)
+     (i/$ "Electorate")
+     (count))
