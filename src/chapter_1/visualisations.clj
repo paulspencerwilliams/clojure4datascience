f371f1d4c6237863c3f3b1679c05a82a4d851b8c
Paul Williams <paul@paulswilliams.me.uk>
2017-03-05 15:55:31 +0000

    Poincare's baker and skewness

--- src/chapter_1/visualisations.clj
+++ src/chapter_1/visualisations.clj
@@ -2,8 +2,9 @@
   (:require   [incanter.core :as i]
               [incanter.charts :as c]
               [incanter.distributions :as d]
+              [incanter.stats :as s]
               [chapter-1.data :refer :all]
-              [chapter-1.descriptivestatistics :as ds]))
+              [chapter-1.descriptivestatistics :refer :all]))
 
 (defn visualise-histograph []
   (-> (i/$ "Electorate" (load-data :uk-scrubbed))
@@ -47,3 +48,37 @@
                      :x-label "Normal distribution"
                      :nbins 20)
         (i/view))))
+
+(defn honest-baker [mean sd]
+  (let [distribution (d/normal-distribution mean sd)]
+    (repeatedly #(d/draw distribution))))
+
+(defn visualise-honest-baker []
+  (-> (take 10000 (honest-baker 1000 30))
+      (c/histogram :x-label "Honest baker"
+                   :nbins 25)
+      (i/view)))
+
+(defn dishonest-baker [mean sd]
+  (let [distribution (d/normal-distribution mean sd)]
+    (->> (repeatedly #(d/draw distribution))
+         (partition 13)
+         (map (partial apply max)))))
+
+(defn visualise-dishonest-baker []
+  (-> (take 10000 (dishonest-baker 950 30))
+      (c/histogram :x-label "Dishonest baker"
+                   :nbins 25)
+      (i/view)))
+
+(defn skew-of-loaves [loaves]
+  (let [weights (take 10000 loaves)]
+    {:mean (mean weights)
+     :median (median weights)
+     :skewness (s/skewness weights)}))
+
+(defn skew-of-dishonest-baker []
+  (skew-of-loaves (dishonest-baker 950 30)))
+
+(defn skew-of-honest-baker []
+  (skew-of-loaves (honest-baker 1000 30)))
